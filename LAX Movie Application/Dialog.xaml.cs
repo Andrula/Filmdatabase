14570decb0eb214f2b17754d1aca4966e5c293f6
Andreas Hjorhøy Rasmussen <88768828+Andrula@users.noreply.github.com>
2022-02-15 14:57:57 +0100

    Add project files.

---
+++ LAX Movie Application/Dialog.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using LAX_Movie_Application;
+
+namespace LAX_Movie_Application
+{
+    /// <summary>
+    /// Interaction logic for Dialog.xaml
+    /// </summary>
+    public partial class Dialog : Window
+    {
+        private string error;
+
+        public Dialog()
+        {
+            InitializeComponent();
+        }
+
+        private void FlytApplikationRundt(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void LukApplikation(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Minimer(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                this.WindowState = WindowState.Minimized;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void MinimerMaximer(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                if (this.WindowState == System.Windows.WindowState.Maximized)
+                    this.WindowState = System.Windows.WindowState.Normal;
+                else
+                    this.WindowState = System.Windows.WindowState.Maximized;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        public void OpretNyFilm(object sender, RoutedEventArgs e)
+        {
+            string ConString = ConfigurationManager.ConnectionStrings["ConString"].ConnectionString;
+
+            SqlCommand cmd = new SqlCommand();
+
+            using (SqlConnection con = new SqlConnection(ConString))
+            {
+                string Query = "Insert into Movies (Titel,Instructor,DateOfRelease) Values('"+ TitelTB.Text + "','" + InstructorTB.Text + "'," + YearTB.Text + ")";
+                SqlCommand command = new SqlCommand(Query, con);
+                try
+                {
+                    con.Open();
+                    command.ExecuteNonQuery();
+                    con.Close();
+                    Close();
+                    MessageBox.Show("Film tilføjet til databasen");
+                }
+                catch (InvalidCastException)
+                {
+                    // recover from exception
+                    MessageBox.Show("Noget gik galt, prøv igen!");
+                }
+
+            }
+        }
+
+        private void SletFilm(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ LAX Movie Application/MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+
+
+namespace LAX_Movie_Application
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            FillDataGrid();
+        }
+
+        // Funktion der udfylder data i Datagrid.
+        private void FillDataGrid()
+        {
+            string ConString = ConfigurationManager.ConnectionStrings["ConString"].ConnectionString;
+
+            string CmdString = string.Empty;
+
+            using (SqlConnection con = new SqlConnection(ConString))
+
+            {
+
+                CmdString = "SELECT Titel, Instructor, DateOfRelease FROM Movies";
+                SqlCommand cmd = new SqlCommand(CmdString, con);
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable("Movies");
+                sda.Fill(dt);
+                DataGridXAML.ItemsSource = dt.DefaultView;
+
+            }
+        }
+     
+        // Tilføjer funktionen at applikationen kan bevæges rundt på skærmen.
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        // Tilføjer funktionen at applikationen lukker ved tryk på "lukke knappen" 
+        private void Close_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        // Tilføjer funktionen at applikationen man minimeres
+        private void Ellipse_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                this.WindowState = WindowState.Minimized;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        // Tilføjer funktionen at applikationen kan gøres mindre, og større hvis den i forvejen er mindre.
+        private void Ellipse_MouseLeftButtonDown_1(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                if (this.WindowState == System.Windows.WindowState.Maximized)
+                    this.WindowState = System.Windows.WindowState.Normal;
+                else
+                    this.WindowState = System.Windows.WindowState.Maximized;
+            }
+            catch  (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        // Tilføjer funktionen at applikationen åbner "opretteleses" vinduet. Den kalder en anden metode.
+        private void Button_Click_Opret(object sender, RoutedEventArgs e)
+        {
+            Dialog nytVindue = new Dialog();
+            nytVindue.Show();
+        }
+
+        private void Button_Click_Detaljer(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_Opdater(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
